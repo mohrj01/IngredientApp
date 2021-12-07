c0f054bc14a167b4af9fa2ce7a72767096c1db6c
mohrj01 <93602315+mohrj01@users.noreply.github.com>
2021-12-06 19:21:15 -0600

    Update ingredient_app.py

--- ingredient_app.py
+++ ingredient_app.py
@@ -18,10 +18,12 @@ st.title('Cookie Recipe Finder')
 st.image('https://leclerc-business-site-production.s3.amazonaws.com/uploads/2018/05/08/13/33/11/09be0c78-9eba-4786-910c-4030e6333bff/header-image.png')
 
 st.write("Jessica Mohr  |  12/08/2021  |  MABA ML Final Project  |  [Github](%s)" % "https://github.com/mohrj01/IngredientApp")
-st.write('By Jessica Mohr')
-st.write('12/8/2021')
-st.write('MABA ML Final Project')
-st.write("[Github](%s)" % "https://github.com/mohrj01/IngredientApp")
+
+col1, col2, col3, col4 = st.columns(4)
+col1.write('By Jessica Mohr')
+col2.write('12/8/2021')
+col3.write('MABA ML Final Project')
+col4.write("[Github](%s)" % "https://github.com/mohrj01/IngredientApp")
 
 
 
